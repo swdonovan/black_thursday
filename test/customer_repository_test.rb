0a7a784808a297f6ded036bfb45bad9d7ba2668b
wratterman <will.ratterman@gmail.com>
2017-06-04 18:58:18 -0600

    tested .all method for customer_repo

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -12,4 +12,15 @@ class CustomerRepositoryTest < Minitest::Test
   def test_it_exists
     assert_instance_of CustomerRepository, setup
   end
+
+  def test_it_returns_all
+    actual = @cstr.all
+    expected = 1000
+
+    assert_equal expected, actual.length
+    assert_instance_of Array, actual
+    assert_equal "Joey", actual[0].first_name
+    assert_equal "Langworth", actual[999].last_name
+    assert_equal 46, actual[45].id
+  end
 end
