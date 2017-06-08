c1a725475b6bfc8c0f933e5a5e899c6f8cab6528
Wil_D <wil.donovan@gmail.com>
2017-06-07 20:17:45 -0600

    partially refactored sales analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -10,20 +10,23 @@ class SalesAnalyst
     @se = se
   end
 
+  def all_merchants
+    @all_merchants = se.merchants.all
+  end
+
   def average_items_per_merchant
-    all_merchants = se.merchants.all
-    find_average(all_merchants)
+    find_average(@all_merchants)
   end
 
   def find_average(all_merchants)
-    numerator = setup_average(all_merchants).inject(0) do |sum, instance|
+    numerator = setup_avg(all_merchants).inject(0) do |sum, instance|
       sum + instance.length
     end
-    average = numerator.to_f / find_denominator(setup_average(all_merchants)).to_f
+    average = numerator.to_f / find_denom(setup_avg(all_merchants)).to_f
     average.round(2)
   end
 
-  def setup_average(all_merchants)
+  def setup_avg(all_merchants)
     merchants = []
     all_merchants.map do |merch|
       merchants << merch.items
@@ -32,9 +35,8 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant_standard_deviation
-    all_merchants = se.merchants.all
-    average = find_average(all_merchants)
-    setup = setup_average(all_merchants)
+    average = find_average(@all_merchants)
+    setup = setup_avg(@all_merchants)
     standard_deviation = standard_deviation_computation(setup, average)
   end
 
@@ -48,8 +50,7 @@ class SalesAnalyst
 
   def merchants_with_high_item_count
     st_d = average_items_per_merchant_standard_deviation
-    all_merchants = se.merchants.all
-    # setup = setup_average_instance(all_merchants)
+    all_merchants = @all_merchants
     find_high_item_merchants(st_d, all_merchants, find_average(all_merchants))
   end
 
@@ -61,7 +62,7 @@ class SalesAnalyst
     merchants
   end
 
-  def setup_average_instance(all_merchants)
+  def setup_avg_instance(all_merchants)
     merchants = []
     all_merchants.map do |merch|
       merchants << merch
@@ -71,16 +72,16 @@ class SalesAnalyst
 
   def average_item_price_for_merchant(id)
     returned_merchant = se.merchants.find_by_id(id)
-    list_of_items = returned_merchant.items
-    average_item_price = (find_numerator(list_of_items)) / (find_denominator(list_of_items))
+    l_o_i = returned_merchant.items
+    average_item_price = (find_num(l_o_i)) / (find_denom(l_o_i))
     average_item_price.round(2)
   end
 
-  def find_numerator(items)
+  def find_num(items)
     items.inject(0) {|sum, item| sum + item.unit_price}
   end
 
-  def find_denominator(items)
+  def find_denom(items)
     if items.length == 0
       return 1
     else items.length
@@ -88,12 +89,11 @@ class SalesAnalyst
   end
 
   def average_average_price_per_merchant
-    all_merchants = se.merchants.all
-    total = find_all_average_prices(all_merchants) / find_denominator(all_merchants)
+    total = price_average
     total.round(2)
   end
 
-  def find_all_average_prices(merchants)
+  def find_average_prices(merchants)
     merchants.inject(0) do |sum, merch|
       sum + average_item_price_for_merchant(merch.id)
     end
@@ -119,24 +119,25 @@ class SalesAnalyst
   end
 
   def find_average_price(all_items)
-    average = find_price_numerator(all_items).to_f / find_denominator(all_items).to_f
+    average = find_price_average(all_items)
     average.round(2)
   end
 
   def find_price_numerator(items)
-    items.inject(0) {|sum, item| sum + item.unit_price_to_dollars}
+    items.inject(0) {|sum, item| sum + item_dollar_price(item)}
   end
 
   def find_golden_items(st_d, setup, average)
     golden_items = []
+    two_sd = (((st_d - average) * 2) + average)
     setup.select do |item|
-      golden_items << item if item.unit_price_to_dollars > (((st_d - average) * 2) + average)
+      golden_items << item if item_dollar_price(item) > two_sd
     end
     golden_items
   end
 
   def average_invoices_per_merchant
-    merchants = se.merchants.all
+    merchants = @all_merchants
     invoices = se.invoices.all
     average = average(invoices.length, merchants.length)
     average.round(2)
@@ -149,7 +150,7 @@ class SalesAnalyst
   end
 
   def merch_invoice_instance
-    all_merchants = se.merchants.all
+    all_merchants = @all_merchants
     all_merchants = all_merchants.map do |merchant|
       merchant.invoices
     end
@@ -159,25 +160,32 @@ class SalesAnalyst
   def top_merchants_by_invoice_count
     std_dev = average_invoices_per_merchant_standard_deviation
     average = average_invoices_per_merchant
+    top_count = two_plus_std_devs(std_dev, average)
+  end
+
+  def two_plus_std_devs(std_dev, average)
     top_count = []
-    se.merchants.all.select do |merchant|
-      if merchant.invoices.length >= (average.to_f + (std_dev*2))
-        top_count << merchant
-      end
+    two_sd = (average.to_f + (std_dev*2))
+    @all_merchants.select do |merchant|
+      top_count << merchant if merchant.invoices.length >= two_sd
     end
     top_count
   end
 
-  def bottom_merchants_by_invoice_count
-    std_dev = average_invoices_per_merchant_standard_deviation
-    average = average_invoices_per_merchant
+  def two_minus_std_devs(std_dev, average)
     bottom_count = []
-    se.merchants.all.select do |merchant|
+    @all_merchants.select do |merchant|
       if merchant.invoices.length <= (average.to_f - (std_dev * 2))
         bottom_count << merchant
       end
     end
     bottom_count
+  end
+
+  def bottom_merchants_by_invoice_count
+    std_dev = average_invoices_per_merchant_standard_deviation
+    average = average_invoices_per_merchant
+    bottom_count = two_minus_std_devs(std_dev, average)
   end
 
   def top_days_by_invoice_count
@@ -201,8 +209,7 @@ class SalesAnalyst
     st_dev = grouped_inv.keys.inject(0) do |sum, day|
       sum + (((grouped_inv[day].length) - average)**2)
     end
-    st_dev = Math.sqrt(st_dev / (grouped_inv.length - 1))
-    st_dev.round(2)
+    st_dev = Math.sqrt(st_dev / (grouped_inv.length - 1)); st_dev.round(2)
   end
 
   def days_of_the_week_average(grouped_inv)
@@ -239,7 +246,6 @@ class SalesAnalyst
   end
 
   def top_customers(total_invoice_sum, x)
-
     total_invoice_sum[0..(x.to_i - 1)]
   end
 
@@ -300,7 +306,6 @@ class SalesAnalyst
     one = one_time.keys.select do |key|
       one_time[key].length == 1
     end
-    binding.pry
     a = se.customers.all
     b = a.select do |customer|
       customer.invoices.all? {|invoice| invoice.is_paid_in_full? == true}
@@ -328,58 +333,78 @@ class SalesAnalyst
       customer.invoices.any? do |invoice|
         invoice.status == :returned
       end
-
     end
   end
 
   def items_bought_in_year(customer_id, year)
-    customer = se.customers.find_by_id(customer_id)
-    invoices = customer.invoices.find_all do |invoice|
-      invoice.created_at.strftime("%Y").to_s == year.to_s
-    end
-    items = invoices.map do |invoice|
-      invoice.items
-    end
+    invoices = find_invoice_year(se.customers.find_by_id(customer_id))
+    items = map_to_items(invoices)
     items.flatten
   end
 
+  def find_invoice_year(customer)
+    customer.invoices.find_all do |invoice|
+      invoice.created_at.strftime("%Y").to_s == year.to_s
+    end
+  end
+
+  def map_to_items(invoices)
+    items = invoices.map do |invoice|
+      invoice.items
+    end
+  end
+
   def highest_volume_items(customer_id)
-    customer = se.customers.find_by_id(customer_id)
-    items_list = customer.invoice_items.flatten
-    items = items_list.group_by do |item|
-      item.item_id
-    end
-    sorted = items.keys.inject({}) do |key, item|
-      key[item] = [items[item][0].quantity]; key
-    end
-    sorted = sorted.keys.select do |key|
-      sorted[key] == sorted.values.max
-    end
+    items_list = (se.customers.find_by_id(customer_id)).invoice_items.flatten
+    items = group_by_item_id(items_list)
+    sorted = regroup_by_quantity(items)
     sorted.map! do |item_ids|
       se.items.find_by_id(item_ids)
     end
   end
 
+  def group_by_item_id(items_list)
+    items_list.group_by do |item|
+      item.item_id
+    end
+  end
+
+  def regroup_by_quantity(items)
+    sorted = items.keys.inject({}) do |key, item|
+      key[item] = [items[item][0].quantity]; key
+    end
+    sorted.keys.select do |key|
+      sorted[key] == sorted.values.max
+    end
+  end
+
   def customers_with_unpaid_invoices
     customer_list = se.customers.all
-
     customer_list.select do |customer|
       customer.invoices.any? {|invoice| invoice.is_paid_in_full? == false}
     end
   end
 
-  def paid_check(invoices)
-    paid_check = invoices.flatten.select do |invoice|
-      invoice if invoice.status == :pending && invoice.is_paid_in_full?
-    end
-    paid_check
-  end
+  # def paid_check(invoices)
+  #   paid_check = invoices.flatten.select do |invoice|
+  #     invoice if invoice.status == :pending && invoice.is_paid_in_full?
+  #   end
+  #   paid_check
+  # end
 
   def best_invoice_by_revenue
     invoice_list = se.invoices.all
+    calculate_invoice_totals(invoice_list)
+  end
+
+  def calculate_invoice_totals(invoice_list)
     invoice_totals = invoice_list.map do |invoice|
       invoice.total.to_f
     end
+    find_top_invoice(invoice_totals, invoice_list)
+  end
+
+  def find_top_invoice(invoice_totals, invoice_list)
     top_invoice = invoice_list.select do |invoice|
       invoice.total.to_f == invoice_totals.max
     end
@@ -388,13 +413,33 @@ class SalesAnalyst
 
   def best_invoice_by_quantity
     invoice_list = se.invoices.all
+    calculate_invoice_quantities(invoice_list)
+  end
+
+  def calculate_invoice_quantities(invoice_list)
     invoice_totals = invoice_list.map do |invoice|
       invoice.total_quantity
     end
+    find_largest_qty_invoice(invoice_list, invoice_totals)
+  end
+
+  def find_largest_qty_invoice(invoice_list, invoice_totals)
     top_invoice = invoice_list.select do |invoice|
       invoice.total_quantity == invoice_totals.max
     end
     top_invoice.shift
+  end
+
+  def find_price_average(all_items)
+    find_price_numerator(all_items).to_f / find_denom(all_items).to_f
+  end
+
+  def price_average
+    find_average_prices(@all_merchants) / find_denom(@all_merchants)
+  end
+
+  def item_dollar_price(item)
+    item.unit_price_to_dollars
   end
 end
 # @sales_engine_dos = SalesEngine.from_csv({
